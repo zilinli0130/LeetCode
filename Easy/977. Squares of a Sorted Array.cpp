e6a2c6944d1e0845c4722b03f3d32e5f80e454bc
Zilin Li <tonyli19970130@gmail.com>
2021-03-03 15:21:30 -0600

    Update 977. Squares of a Sorted Array.cpp

--- Easy/977. Squares of a Sorted Array.cpp
+++ Easy/977. Squares of a Sorted Array.cpp
@@ -1,31 +1,47 @@
-/* This problem implements stack
+/*This problem implements the Quick Sort algorihtm. 
 
-Time : O(N)
-Space : O(N)
+Time: O(Nlog(N))
+Space: O(Nlog(N))
+
 */
 
 class Solution {
 public:
-    bool isValid(string s) {
+    vector<int> sortedSquares(vector<int>& nums) {
         
-         string openingBrackets = "([{";
-         string closingBrackets = ")]}";
-         unordered_map<char,char> matchingBrackets{{')', '('}, {']', '['}, {'}', '{'}};
-         stack<char> stack;
-         
-         for (char element : s) {
-             if (openingBrackets.find(element) != string::npos) {
-                 stack.push(element);
-             } else if (closingBrackets.find(element) != string::npos) {
-                 if (stack.size() == 0) {
-                     return false;
-                 } else if (stack.top() == matchingBrackets[element]) {
-                     stack.pop();
-                 } else {
-                     return false;
-                 }
-             }
-         }
-        return stack.size() == 0;
+        for (int i = 0; i < nums.size(); i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+        quickSort(nums, 0, nums.size() - 1);
+	    return nums;
+    }
+    
+    void quickSort(vector<int> &nums, int startIdx, int endIdx) {
+	
+        if (startIdx >= endIdx) {
+            return;
+        }
+        int pivot = startIdx, left = startIdx + 1, right = endIdx;
+        while (right >= left) {
+            if (nums.at(left) > nums.at(pivot) && nums.at(right) < nums.at(pivot)){
+                swap(nums[left], nums[right]);
+            }
+            if (nums.at(left) <= nums.at(pivot)) {
+                left++;
+            }
+            if (nums.at(right) >= nums.at(pivot)) {
+                right--;
+            }
+        }
+        swap(nums[pivot], nums[right]);
+        
+        bool isLeftSubarraySmaller = right - 1 - startIdx < endIdx - (right + 1);
+        if (isLeftSubarraySmaller) {
+            quickSort(nums, startIdx, right - 1);
+            quickSort(nums, right + 1, endIdx);
+        } else {
+            quickSort(nums, right + 1, endIdx);
+            quickSort(nums, startIdx, right - 1);
+        }
     }
 };
