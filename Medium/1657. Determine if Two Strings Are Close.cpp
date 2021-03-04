c2367497a261d43a6836a3f453f6eb1dc1a1ec09
Zilin Li <tonyli19970130@gmail.com>
2021-03-04 11:37:15 -0600

    Update 1657. Determine if Two Strings Are Close.cpp

--- Medium/1657. Determine if Two Strings Are Close.cpp
+++ Medium/1657. Determine if Two Strings Are Close.cpp
@@ -1,36 +1,53 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+/*This problem impements greedy algorithm with four times of sort()
+We need to ensure that both words has the same letters with the same counts
+Time : O(Nlog(N) + Mlog(M))
+Space : O(X + Y)
+N is the total number of letters in word1
+M is the total number of letters in word2
+X is the number of same letters in word1
+Y is the number of same letters in word2
+*/
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *newList = new ListNode(0);
-        ListNode *p1 = l1;
-        ListNode *p2 = l2;
-        ListNode *p3 = newList;
-        int extraDigit = 0;
+    bool closeStrings(string word1, string word2) {
         
-        while (p1 != NULL || p2 != NULL) {
-            
-            int p1Val = p1 != NULL ? p1->val : 0;
-            int p2Val = p2 != NULL ? p2->val : 0;
-            int totalDigitSum = p1Val + p2Val + extraDigit;
-            int newListVal = totalDigitSum >= 10 ? totalDigitSum - 10 : totalDigitSum;
-            extraDigit = totalDigitSum >= 10 ? 1 : 0;
-            
-            p3->next = new ListNode(newListVal);
-            p3 = p3->next;
-            if (p1 != NULL) {p1 = p1->next;}
-            if (p2 != NULL) {p2 = p2->next;}
+        if(word1.size() != word2.size()) {
+            return false;
         }
-        if (extraDigit == 1) {p3->next = new ListNode(1);}
-       return newList->next; 
+        
+        sort(word1.begin(), word1.end());
+        sort(word2.begin(), word2.end());
+        
+        unordered_map<char, int> hashmap1, hashmap2;
+        vector<int> word1Count, word2Count;
+        vector<char> word1Char, word2Char;
+        for(char letter: word1) {
+            hashmap1[letter]++;
+        }
+        for(char letter : word2) {
+            hashmap2[letter]++;
+        }
+        for(auto i : hashmap1) {
+            word1Char.push_back(i.first);
+            word1Count.push_back(i.second);
+        }
+        for(auto i : hashmap2) {
+            word2Char.push_back(i.first);
+            word2Count.push_back(i.second);
+        }
+        
+        if (word1Count.size() != word2Count.size() || word1Char.size() != word2Char.size()) {
+            return false;
+        }
+        
+        vector<int> sortedWord1Count(word1Count), sortedWord2Count(word2Count);
+	    sort(sortedWord1Count.begin(), sortedWord1Count.end());
+        sort(sortedWord2Count.begin(), sortedWord2Count.end());
+        for (int i = 0; i < sortedWord1Count.size(); i++) {
+            if(sortedWord1Count[i] != sortedWord2Count[i] || word1Char[i] != word2Char[i]) {
+                return false;
+            }
+        }
+      return true;  
     }
 };
