4400717d604814f4a3d98b918468201efc1d4d5e
Zilin Li <tonyli19970130@gmail.com>
2021-03-04 11:53:12 -0600

    Update 312. Burst Balloons.cpp

--- Hard/312. Burst Balloons.cpp
+++ Hard/312. Burst Balloons.cpp
@@ -1,36 +1,23 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *newList = new ListNode(0);
-        ListNode *p1 = l1;
-        ListNode *p2 = l2;
-        ListNode *p3 = newList;
-        int extraDigit = 0;
+    int maxCoins(vector<int>& nums) {
         
-        while (p1 != NULL || p2 != NULL) {
-            
-            int p1Val = p1 != NULL ? p1->val : 0;
-            int p2Val = p2 != NULL ? p2->val : 0;
-            int totalDigitSum = p1Val + p2Val + extraDigit;
-            int newListVal = totalDigitSum >= 10 ? totalDigitSum - 10 : totalDigitSum;
-            extraDigit = totalDigitSum >= 10 ? 1 : 0;
-            
-            p3->next = new ListNode(newListVal);
-            p3 = p3->next;
-            if (p1 != NULL) {p1 = p1->next;}
-            if (p2 != NULL) {p2 = p2->next;}
+        //Padding on both sides
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+        
+        //Create a cache
+        vector<vector<int>> cache(nums.size() + 2, vector<int>(nums.size() + 2, 0));
+        
+        for (int l = 1; l <= nums.size(); l++) {
+            for (int i = 1; i <= nums.size() - l + 1; i++) {
+                int j = i + l - 1;
+                for (int k = i; k <= j; k++) {
+                    cache[i][j] = max(cache[i][j], cache[i][k - 1] + cache[k + 1][j] 
+                                      + nums[i - 1]*nums[k]*nums[j + 1]);
+                }
+            }
         }
-        if (extraDigit == 1) {p3->next = new ListNode(1);}
-       return newList->next; 
+       return cache[1][nums.size()]; 
     }
 };
