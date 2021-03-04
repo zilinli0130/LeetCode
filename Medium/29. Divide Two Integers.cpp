f015a50084f64db18ac34f72e84639e264a9d447
Zilin Li <tonyli19970130@gmail.com>
2021-03-04 11:40:01 -0600

    Update 29. Divide Two Integers.cpp

--- Medium/29. Divide Two Integers.cpp
+++ Medium/29. Divide Two Integers.cpp
@@ -1,36 +1,32 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+/*
+This problem is all about math
+log(a/b) = log(a) - log(b)
+a/b = exp(log(a) - log(b))
+
+Time : O(1)
+Space : O(1)
+*/
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *newList = new ListNode(0);
-        ListNode *p1 = l1;
-        ListNode *p2 = l2;
-        ListNode *p3 = newList;
-        int extraDigit = 0;
+    int divide(int dividend, int divisor) {
         
-        while (p1 != NULL || p2 != NULL) {
-            
-            int p1Val = p1 != NULL ? p1->val : 0;
-            int p2Val = p2 != NULL ? p2->val : 0;
-            int totalDigitSum = p1Val + p2Val + extraDigit;
-            int newListVal = totalDigitSum >= 10 ? totalDigitSum - 10 : totalDigitSum;
-            extraDigit = totalDigitSum >= 10 ? 1 : 0;
-            
-            p3->next = new ListNode(newListVal);
-            p3 = p3->next;
-            if (p1 != NULL) {p1 = p1->next;}
-            if (p2 != NULL) {p2 = p2->next;}
-        }
-        if (extraDigit == 1) {p3->next = new ListNode(1);}
-       return newList->next; 
+        int sign = (dividend > 0) ^ (divisor > 0);
+        
+        /*XOR Truth Table
+        //1  1 -> 0
+        //1  0 -> 1
+        //0  1 -> 1
+        //0  0 -> 0
+        */
+        
+        long length = abs(dividend);
+        long interval = abs(divisor);
+        long quotient = exp(log(length) - log(interval));
+        
+        quotient *= (sign == 1 ? -1 : 1);
+        
+        if(quotient > INT_MAX) {return INT_MAX;}
+        if(quotient < INT_MIN) {return INT_MIN;}
+        return quotient;
     }
 };
