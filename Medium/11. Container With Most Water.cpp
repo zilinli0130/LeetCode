08b674aeb7ceffb1d2c7bcca4d2ba7df2b0d585d
Zilin Li <tonyli19970130@gmail.com>
2021-03-04 11:35:27 -0600

    Update 11. Container With Most Water.cpp

--- Medium/11. Container With Most Water.cpp
+++ Medium/11. Container With Most Water.cpp
@@ -1,36 +1,29 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+/*
+This problem implements the Two Pointer method. The max area is equal to the min length between these two pointers multiplying 
+with the distance between those two pointers
+
+Time : O(N)
+Space : O(1)
+Where N is the total number of elements inside the given array
+
+*/
+
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *newList = new ListNode(0);
-        ListNode *p1 = l1;
-        ListNode *p2 = l2;
-        ListNode *p3 = newList;
-        int extraDigit = 0;
+    int maxArea(vector<int>& height) {
         
-        while (p1 != NULL || p2 != NULL) {
+        int left = 0, right = height.size() - 1, maxArea = 0;
+        
+        for (int i = 0; i < height.size(); i++) {
             
-            int p1Val = p1 != NULL ? p1->val : 0;
-            int p2Val = p2 != NULL ? p2->val : 0;
-            int totalDigitSum = p1Val + p2Val + extraDigit;
-            int newListVal = totalDigitSum >= 10 ? totalDigitSum - 10 : totalDigitSum;
-            extraDigit = totalDigitSum >= 10 ? 1 : 0;
-            
-            p3->next = new ListNode(newListVal);
-            p3 = p3->next;
-            if (p1 != NULL) {p1 = p1->next;}
-            if (p2 != NULL) {p2 = p2->next;}
+            if (height[left] < height[right]) {
+                maxArea = max(maxArea, height[left] * (right - left));
+                left++;
+            } else {
+                maxArea = max(maxArea, height[right] * (right - left));
+                right--;
+            }
         }
-        if (extraDigit == 1) {p3->next = new ListNode(1);}
-       return newList->next; 
+        return maxArea;
     }
 };
