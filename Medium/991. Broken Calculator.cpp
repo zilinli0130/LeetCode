76c1d02c89327f26e7d0d21d6bb5440e44c98b31
Zilin Li <tonyli19970130@gmail.com>
2021-03-04 11:51:51 -0600

    Update 991. Broken Calculator.cpp

--- Medium/991. Broken Calculator.cpp
+++ Medium/991. Broken Calculator.cpp
@@ -1,36 +1,33 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+/*
+We could calclate the number of steps backward starting from Y:
+
+if Y is odd and Y > X : Y++
+if Y is even and Y > X: Y /= 2
+if Y < X: Y++
+
+Time : O(N)
+Space : O(1)
+*/
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *newList = new ListNode(0);
-        ListNode *p1 = l1;
-        ListNode *p2 = l2;
-        ListNode *p3 = newList;
-        int extraDigit = 0;
+    int brokenCalc(int X, int Y) {
         
-        while (p1 != NULL || p2 != NULL) {
+        if (X == Y) {return 0;}
+        
+        int res = 0;
+        while (Y > X) {
             
-            int p1Val = p1 != NULL ? p1->val : 0;
-            int p2Val = p2 != NULL ? p2->val : 0;
-            int totalDigitSum = p1Val + p2Val + extraDigit;
-            int newListVal = totalDigitSum >= 10 ? totalDigitSum - 10 : totalDigitSum;
-            extraDigit = totalDigitSum >= 10 ? 1 : 0;
-            
-            p3->next = new ListNode(newListVal);
-            p3 = p3->next;
-            if (p1 != NULL) {p1 = p1->next;}
-            if (p2 != NULL) {p2 = p2->next;}
+            if (Y % 2 == 1) {
+                Y++;
+            } else {
+                Y /= 2;
+            }
+            res++;
         }
-        if (extraDigit == 1) {p3->next = new ListNode(1);}
-       return newList->next; 
+    return res + X - Y;
     }
 };
+
+
+
+
