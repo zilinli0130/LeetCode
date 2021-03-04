6e9c2de248fa2d9d11bb316f20e4c094017cf670
Zilin Li <tonyli19970130@gmail.com>
2021-03-04 11:41:05 -0600

    Update 334. Increasing Triplet Subsequence.py

--- Medium/334. Increasing Triplet Subsequence.py
+++ Medium/334. Increasing Triplet Subsequence.py
@@ -1,35 +1,20 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
-#This problem implements three pointer method
-#We need to keep track of each pointer and make
-#sure the update order of pointers are correct
-#
-#Time : O(N) where N is the number of nodes in a linked list
+#We keep updating the value of idx1 and idx2. If we
+#find a value which is greater than idx2, we could
+#simply return True.
+#Time : O(N)
 #Space : O(1)
 
-class Solution(object):
-    def swapPairs(self, head):
-        if not head or not head.next:
-            return head
+class Solution:
+    def increasingTriplet(self, nums):
         
-        fakeNode = ListNode(0)
-        p1 = fakeNode
-        p2 = head
+        idx1 = float("inf")
+        idx2 = float("inf")
         
-        while p2 is not None and p2.next is not None:
-            
-            #Swap
-            p3 = p2.next
-            p1.next = p3
-            p2.next = p3.next
-            p3.next = p2
-            
-            #Update
-            p1 = p2
-            p2 = p2.next
-        return fakeNode.next
-        
+        for num in nums:
+            if num < idx1: 
+                idx1 = num
+            if num < idx2 and num > idx1: 
+                idx2 = num
+            if num > idx2: 
+                return True
+        return False
