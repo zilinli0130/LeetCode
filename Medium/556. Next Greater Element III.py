969a73050e2cbf8cc67e14d3aa6a14584bf8725e
Zilin Li <tonyli19970130@gmail.com>
2021-03-04 11:45:00 -0600

    Update 556. Next Greater Element III.py

--- Medium/556. Next Greater Element III.py
+++ Medium/556. Next Greater Element III.py
@@ -1,35 +1,29 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
-#This problem implements three pointer method
-#We need to keep track of each pointer and make
-#sure the update order of pointers are correct
-#
-#Time : O(N) where N is the number of nodes in a linked list
+#Time : O(N)
 #Space : O(1)
 
-class Solution(object):
-    def swapPairs(self, head):
-        if not head or not head.next:
-            return head
+class Solution:
+    def nextGreaterElement(self, n):
+        s = list(str(n))
         
-        fakeNode = ListNode(0)
-        p1 = fakeNode
-        p2 = head
-        
-        while p2 is not None and p2.next is not None:
+        #Find the first idx in which it is greater than its previous idx
+        idx1 = len(s) - 1
+        while idx1 > 0:
+            if s[idx1] > s[idx1-1]:
+                break
+            idx1 -= 1
             
-            #Swap
-            p3 = p2.next
-            p1.next = p3
-            p2.next = p3.next
-            p3.next = p2
-            
-            #Update
-            p1 = p2
-            p2 = p2.next
-        return fakeNode.next
+        if idx1 == 0: 
+            return -1
         
+        #Find the first idx in which it is greater than idx1 and then swap
+        idx2 = len(s) - 1
+        while idx2 >= idx1:
+            if s[idx2] > s[idx1-1]:
+                s[idx1-1], s[idx2] = s[idx2], s[idx1-1]
+                break
+            idx2 -= 1
+        
+        s[idx1:] = s[idx1:][::-1]
+        result = int(''.join(s))
+        
+        return result if -2**31 - 1 <= result <= 2**31 - 1 else -1 #signed int32
